2a74e8064203a19a36c12fdb5882452d845caf4b
dreemsoul <vanilla.guinness@aol.com>
2013-11-03 18:20:13 -0500

    Update main.c

--- main.c
+++ main.c
@@ -66,12 +66,26 @@ static void destroy( GtkWidget *widget,
 // Unmount
 static void unmount (GtkWidget *wid, GtkWidget *win)
 {
-  system("gksu umount /mnt/");
-  GtkWidget *dialog = NULL;
-  dialog = gtk_message_dialog_new (GTK_WINDOW (win), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_CLOSE, "Unmounted");
-  gtk_window_set_position (GTK_WINDOW (dialog), GTK_WIN_POS_CENTER);
-  gtk_dialog_run (GTK_DIALOG (dialog));
-  gtk_widget_destroy (dialog);
+  int checkfail;
+
+  checkfail = system("gksu umount /mnt/");
+
+  if (checkfail == 0)
+  {
+     GtkWidget *dialog = NULL;
+     dialog = gtk_message_dialog_new (GTK_WINDOW (win), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_CLOSE, "Unmounted");
+     gtk_window_set_position (GTK_WINDOW (dialog), GTK_WIN_POS_CENTER);
+     gtk_dialog_run (GTK_DIALOG (dialog));
+     gtk_widget_destroy (dialog);
+  }
+  else
+  {
+     GtkWidget *dialog = NULL;
+     dialog = gtk_message_dialog_new (GTK_WINDOW (win), GTK_DIALOG_MODAL, GTK_MESSAGE_INFO, GTK_BUTTONS_CLOSE, "Device Couldn't Unmount\nYou may want to check\nthe folder.\n");
+     gtk_window_set_position (GTK_WINDOW (dialog), GTK_WIN_POS_CENTER);
+     gtk_dialog_run (GTK_DIALOG (dialog));
+     gtk_widget_destroy (dialog);
+  }
 }
 
 //Mount
